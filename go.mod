e949c62109326314876f141608c2efd50a029988
justin <justin.t.connolly@gmail.com>
2023-12-23 17:41:59 -0600

    Had to do it to 'em

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/justintconnolly/hello-world
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Nice to meet you all!")
+	fmt.Println("I'll be here trying to build things with Go.")
+	fmt.Println("I doubt much of it will be useful, but I hope it'll be fun and I'll learn a thing or two :)")
+}
